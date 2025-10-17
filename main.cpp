e8ed145aa98b58e22c09b7c7d670356829dc68af
Rishit Gupta <57046026+Rashod-coder@users.noreply.github.com>
2025-10-17 16:30:22 -0700

    added comments to code

--- main.cpp
+++ main.cpp
@@ -27,39 +27,31 @@ int main() {
 
     
     while (true) {
-
-        // Check if GLV system and shutdown circuit are active
+        // Check if GLV system and shutdown circuit are active 
         if (shutdown_circuit.read()) {
         Tractive_System_Active = true;  
         } else {
         Tractive_System_Active = false;  
         }
 
+        //Reads Acclerator Pedal Position Sensor Voltages
         float apps0_value = APPS0.read() * 3.3f; 
         float apps1_value = APPS1.read() * 3.3f; 
 
-        
-
+        //Reads Brake Sensor and Cockpit Switch States
         float brake_value = brake_sensor.read();
         float brake_voltage = brake_sensor.read() * 3.3f;
+        float brake_percent = brake_value * 100.0f;
         int cockpit_switch_state = cockpit_switch.read();
 
+        // Calculates percentage & averages it out
         float pos0 = (apps0_value - APPS0_MIN) / (APPS0_MAX - APPS0_MIN) * 100.0f;
         float pos1 = (apps1_value - APPS1_MIN) / (APPS1_MAX - APPS1_MIN) * 100.0f;
-        float brake_percent = brake_value * 100.0f;
         float average_pos = (pos0 + pos1) / 2.0f;
+
         
 
-        // If position goes out of ranges prints implausible. 
-        if (pos0 <0.0f) printf("Implausible\n");
-        if (pos0>100.0f) printf("Implausible\n");
-        if (pos1<0.0f) printf("Implausible\n");
-        if (pos1>100.0f) printf("Implausible\n");
-
-
-        // Checks for implausibility condition
-        float diff = fabs(pos0 - pos1);
-
+        
         // Ready to Drive functionality
         if (brake_percent > 80.0f) {
             isBrakePressed = true;
@@ -82,6 +74,13 @@ int main() {
         else {
                 printf("APPS0 Position:  %f, APPS1 Position: %f\n", pos0, pos1);
                 printf("Average Position: %f\n", average_pos);
+
+                // If position goes out of ranges prints implausible. 
+                if (pos0 <0.0f || pos0 > 100.0f) printf("Implausible\n");
+                if (pos1 <0.0f || pos1 >100.0f) printf("Implausible\n");
+                
+                // Implausibility Detection
+                float diff = fabs(pos0 - pos1);
                 if (diff > 10.0f) {
                     if (!implausibility_detected) {
                         implausibility_timer.start();
@@ -115,10 +114,6 @@ int main() {
             bse_timer.reset();
             bse_detected = false;
         }
-
-        // wheel slip
-
-        
     
         ThisThread::sleep_for(100ms);
     
